a2ebcf61caa69d103760ef8749e6742554ad616a
EE-noob <98376102+EE-noob@users.noreply.github.com>
2025-02-09 15:24:53 +0000

    finish

--- app.py
+++ app.py
@@ -296,4 +296,4 @@ def sort_warriors():
 #     app.run(debug=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
